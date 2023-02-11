924ad79affa8eaf61d2d858a5bcdb8f02942d59d
HARSH DUBEY <amanad08052@gmail.com>
2023-02-11 10:25:37 +0530

    initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,6 @@ import {
   AiOutlineUser,
 } from "react-icons/ai";
 
-import { CgFileDocument } from "react-icons/cg";
 
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
@@ -68,16 +67,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-           
-            <Nav.Item>
-               <Nav.Link
-                as={Link}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item>
+          
 
             <Nav.Item className="fork-btn">
               <Button
